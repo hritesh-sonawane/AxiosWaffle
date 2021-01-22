a5642c1ee1b312e0b9e86b4881142c0d2ef08301
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-22 23:02:01 +0530

    render fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,20 +7,28 @@ import axios from 'axios';
 
 class Blog extends Component {
 
+  state = {
+    posts: []
+  }
+
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response);
+        this.setState({posts: response.data});
+        // console.log(response);
       });
   }
 
   render () {
+
+    const posts = this.state.posts.map(post => {
+      return <Post title={post.title} key={post.id} />
+    });
+
     return (
       <div>
         <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
+          {posts}
         </section>
         <section>
           <FullPost />
