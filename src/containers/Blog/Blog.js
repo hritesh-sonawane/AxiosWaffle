1d6072d6ff0c5f0c5cf84dcbaf557503648e4e17
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-22 23:35:32 +0530

    data transform commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,15 +14,21 @@ class Blog extends Component {
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        this.setState({posts: response.data});
-        // console.log(response);
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Sasuke'
+          }
+        });
+        this.setState({posts: updatedPosts});
       });
   }
 
   render () {
 
     const posts = this.state.posts.map(post => {
-      return <Post title={post.title} key={post.id} />
+      return <Post title={post.title} author={post.author} key={post.id} />
     });
 
     return (
