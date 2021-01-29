2b2d41598299b4f8935cf6f5b0b4d38f798e33f1
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-29 23:28:10 +0530

    switching bw pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import './Blog.css';
 // import axios from '../../axios';
 import Posts from '../Posts/Posts';
 import { Route } from 'react-router-dom';
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
   render () {
@@ -19,6 +20,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>221B</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
